2aafde5d755988404dc4129f64f5cba6a5840e9b
Shawn Sullivan <sullivansm3@gmail.com>
2020-08-12 12:47:57 -0600

    Create mongoose db queries for GET & POST routes

---
+++ src/server/database/queries.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/linqpal', { useNewUrlParser: true, useUnifiedTopology: true });
+const User = require('./userModel');
+
+const db = mongoose.connection;
+
+const getUserData = async (username) => {
+  return await User.find({ username: username });
+}
+
+const createNewUser = async (userData) => {
+    try {
+      const newUser = new User(userData);
+      await newUser.save();
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+}
+
+db.on('error', () => {
+  console.log('mongoose connection error');
+});
+
+db.once('open', () => {
+  console.log('mongoose connected successfully');
+});
+
+module.exports = {
+  getUserData,
+  createNewUser,
+}
