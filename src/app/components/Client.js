86ebc88c9ce3f95c664936b42d8b21bcee5902be
Shawn Sullivan <sullivansm3@gmail.com>
2020-08-12 14:28:05 -0600

    Create Client facing form

--- src/app/components/Client.js
+++ src/app/components/Client.js
@@ -1,12 +1,87 @@
-import React, { useState } from 'react';
- 
-function Client(props) {
+import React from 'react';
 
-  return (
-    <div>
-      Client page
-    </div>
-  );
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+ 
+class Client extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      phoneNumber: '',
+      address: '',
+      city: '',
+      state: '',
+      zip: null,
+      SSN: '',
+      isAdmin: false,
+    }
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(e) {
+    e.preventDefault();
+    let { value, type, id } = e.target;
+    this.setState({[id]: type === 'number' ? parseFloat(value) : value});
+  }
+
+  render() {
+    return (
+      <Form className='form'>
+        <Form.Row>
+          <Form.Group as={Col} controlId="firstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="text" placeholder="Jane" onChange={this.handleInput}/>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="lastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="text" placeholder="Doe" onChange={this.handleInput}/>
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Row>
+          <Form.Group as={Col} controlId="phoneNumber">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control type="text" placeholder="123-456-7890" onChange={this.handleInput}/>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="SSN">
+            <Form.Label>Social Security Number</Form.Label>
+            <Form.Control type="text" placeholder="123-45-6789" onChange={this.handleInput}/>
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Group controlId="address" id="address">
+          <Form.Label>Address</Form.Label>
+          <Form.Control type ="text" placeholder="1234 Main St" onChange={this.handleInput} />
+        </Form.Group>
+
+        <Form.Row>
+          <Form.Group as={Col} controlId="city">
+            <Form.Label>City</Form.Label>
+            <Form.Control placeholder="Boulder" onChange={this.handleInput}/>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="state">
+            <Form.Label>State</Form.Label>
+            <Form.Control placeholder="Colorado" onChange={this.handleInput}/>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="zip">
+            <Form.Label>Zip</Form.Label>
+            <Form.Control type="number" placeholder="80301" onChange={this.handleInput}/>
+          </Form.Group>
+        </Form.Row>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    );
+  }
 }
  
 export default Client;
