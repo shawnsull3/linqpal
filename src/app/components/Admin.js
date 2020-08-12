381ec34756ec96d19a0b31ce8b88b78568e95677
Shawn Sullivan <sullivansm3@gmail.com>
2020-08-12 17:57:47 -0600

    Render user info for admin user

--- src/app/components/Admin.js
+++ src/app/components/Admin.js
@@ -1,12 +1,58 @@
-import React, { useState } from 'react';
+import React from 'react';
+import axios from 'axios';
+import Table from 'react-bootstrap/Table'
  
-function Admin(props) {
+class Admin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInfo: [],
+    }
+  }
 
-  return (
-    <div>
-      Admin page
-    </div>
-  );
+  componentDidMount() {
+    axios.get('http://localhost:4000/allUserData')
+      .then( userData => {
+        console.log(userData.data)
+        this.setState({userInfo: userData.data})
+      })
+      .catch((err) => {
+          console.log(err);
+      });
+  }
+
+  render() {
+    const { userInfo } = this.state;
+    return (
+      <div>
+        <h4>User Infomation</h4>
+        <Table striped bordered hover variant="dark" className='user-table'>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Phone Number</th>
+              <th>Address</th>
+              <th>Social Security Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userInfo.map( (user, i) => (
+              <tr key={i+1}>
+                <td>{i+1}</td>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.phoneNumber}</td>
+                <td>{`${user.address}, ${user.city}, ${user.state} ${user.zip}`}</td>
+                <td>{user.SSN}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
  
 export default Admin;
