59a724e3bb1befa9cb90792ac7578cda55521c1a
Shawn Sullivan <sullivansm3@gmail.com>
2020-08-12 10:14:41 -0600

    Redirect client to login intially w react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/App.css';
-
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+ 
+import Login from './Login';
+ 
 function App() {
   return (
     <div className="App">
-      hello world
+      <BrowserRouter>
+          <Switch>
+            <Route exact path="/" render={() => <Redirect to="/login" />} />
+            <Route path="/login" component={Login} />
+          </Switch>
+      </BrowserRouter>
     </div>
   );
 }
-
+ 
 export default App;
