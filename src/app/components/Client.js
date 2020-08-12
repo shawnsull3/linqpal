5115c68287430229ae11505677f4d8c0a81d67c3
Shawn Sullivan <sullivansm3@gmail.com>
2020-08-12 15:07:00 -0600

    Psuedocode handlesubmit. Add button for admin login

--- src/app/components/Client.js
+++ src/app/components/Client.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+
+import axios from 'axios';
  
 class Client extends React.Component {
   constructor(props) {
@@ -19,6 +20,7 @@ class Client extends React.Component {
       isAdmin: false,
     }
     this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleInput(e) {
@@ -27,59 +29,72 @@ class Client extends React.Component {
     this.setState({[id]: type === 'number' ? parseFloat(value) : value});
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('submitted')
+    // encrypt SSN
+    // axios post to server / database
+  }
+
   render() {
     return (
-      <Form className='form'>
-        <Form.Row>
-          <Form.Group as={Col} controlId="firstName">
-            <Form.Label>First Name</Form.Label>
-            <Form.Control type="text" placeholder="Jane" onChange={this.handleInput}/>
+      <div>
+        <Form className='form' onSubmit={this.handleSubmit}>
+          <Form.Row>
+            <Form.Group as={Col} controlId="firstName">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control type="text" placeholder="Jane" onChange={this.handleInput}/>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="lastName">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control type="text" placeholder="Doe" onChange={this.handleInput}/>
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Row>
+            <Form.Group as={Col} controlId="phoneNumber">
+              <Form.Label>Phone Number</Form.Label>
+              <Form.Control type="text" placeholder="123-456-7890" onChange={this.handleInput}/>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="SSN">
+              <Form.Label>Social Security Number</Form.Label>
+              <Form.Control type="text" placeholder="123-45-6789" onChange={this.handleInput}/>
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Group controlId="address" id="address">
+            <Form.Label>Address</Form.Label>
+            <Form.Control type ="text" placeholder="1234 Main St" onChange={this.handleInput} />
           </Form.Group>
 
-          <Form.Group as={Col} controlId="lastName">
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control type="text" placeholder="Doe" onChange={this.handleInput}/>
-          </Form.Group>
-        </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} controlId="city">
+              <Form.Label>City</Form.Label>
+              <Form.Control placeholder="Boulder" onChange={this.handleInput}/>
+            </Form.Group>
 
-        <Form.Row>
-          <Form.Group as={Col} controlId="phoneNumber">
-            <Form.Label>Phone Number</Form.Label>
-            <Form.Control type="text" placeholder="123-456-7890" onChange={this.handleInput}/>
-          </Form.Group>
+            <Form.Group as={Col} controlId="state">
+              <Form.Label>State</Form.Label>
+              <Form.Control placeholder="Colorado" onChange={this.handleInput}/>
+            </Form.Group>
 
-          <Form.Group as={Col} controlId="SSN">
-            <Form.Label>Social Security Number</Form.Label>
-            <Form.Control type="text" placeholder="123-45-6789" onChange={this.handleInput}/>
-          </Form.Group>
-        </Form.Row>
+            <Form.Group as={Col} controlId="zip">
+              <Form.Label>Zip</Form.Label>
+              <Form.Control type="number" placeholder="80301" onChange={this.handleInput}/>
+            </Form.Group>
+          </Form.Row>
 
-        <Form.Group controlId="address" id="address">
-          <Form.Label>Address</Form.Label>
-          <Form.Control type ="text" placeholder="1234 Main St" onChange={this.handleInput} />
-        </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
 
-        <Form.Row>
-          <Form.Group as={Col} controlId="city">
-            <Form.Label>City</Form.Label>
-            <Form.Control placeholder="Boulder" onChange={this.handleInput}/>
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="state">
-            <Form.Label>State</Form.Label>
-            <Form.Control placeholder="Colorado" onChange={this.handleInput}/>
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="zip">
-            <Form.Label>Zip</Form.Label>
-            <Form.Control type="number" placeholder="80301" onChange={this.handleInput}/>
-          </Form.Group>
-        </Form.Row>
-
-        <Button variant="primary" type="submit">
-          Submit
+        <Button variant="primary" className="login-btn" onClick={() => this.props.history.push('/login')} >
+          Admin Login
         </Button>
-      </Form>
+      </div>
     );
   }
 }
