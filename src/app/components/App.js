0745d5f3fd2fd46f4cfda24b1e992587e85ff4c3
Shawn Sullivan <sullivansm3@gmail.com>
2020-08-12 10:51:24 -0600

    Add routes for admin & client

---
+++ src/app/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../styles/App.css';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+ 
+import Login from './Login';
+import Admin from './Admin';
+import Client from './Client';
+ 
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+          <Switch>
+            <Route exact path="/" render={() => <Redirect to="/login" />} />
+            <Route path="/login" component={Login} />
+            <Route path="/admin" component={Admin} />
+            <Route path="/home" component={Client} />
+          </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+ 
+export default App;
