64e3ea725c1acd586417420131f0e49b2c818371
Shawn Sullivan <sullivansm3@gmail.com>
2020-08-13 12:27:59 -0600

    GET & POST routes for userData & newUser

--- src/server/server.js
+++ src/server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
+const db = require('./database/queries');
  
 const app = express();
 const port = process.env.PORT || 4000;
@@ -12,6 +13,28 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/allUserData', async (req, res) => {
+    try {
+      const userData = await db.getAllUserData();
+      res.send(userData);
+    } 
+    catch (error) {
+      console.log(error);
+      res.sendStatus(400);
+    }
+});
+
+app.post('/newUser', async (req, res) => {
+    try {
+      await db.createNewUser(req.body);
+      res.sendStatus(201);
+    } 
+    catch (error) {
+      console.log(error);
+      res.sendStatus(400);
+    }
+});
  
 app.listen(port, () => {
   console.log('Server started on: ' + port);
